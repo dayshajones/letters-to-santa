e812cbb317510cf16bdd56c31e906f1a021249cb
“Daysha <+>
2021-11-09 13:55:09 -0800

    added Letter and Elf migration

---
+++ db/migrate/20211109215149_create_letters.rb
@@ -0,0 +1,9 @@
+class CreateLetters < ActiveRecord::Migration[6.1]
+  def change
+    create_table :letters do |t|
+      t.integer :user_id
+      t.integer :elf_id
+      t.timestamps
+    end
+  end
+end
