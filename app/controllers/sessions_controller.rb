1061784950929687f4983eb5e33c2c0e931a8b1b
“Daysha <+>
2021-11-15 11:52:15 -0800

    changed login route

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,23 @@
 class SessionsController < ApplicationController
+
+    def new
+
+    end
+
+    def create
+        user = User.find_by_email(params[:email])
+        if user && user.authenticate(params[:password])
+            session[:user_id] = user.id 
+            redirect_to letters_path
+        else
+            flash.now.alert = "Invalid email or password"
+            render "new"
+        end
+    end
+
+
+    def logout
+        session.clear
+        redirect_to new_user_path
+    end
 end
