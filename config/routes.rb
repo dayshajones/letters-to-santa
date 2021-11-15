b181404c770a65ae91e82dccd2b43a772f95e4de
“Daysha <+>
2021-11-15 15:38:09 -0800

    added root route and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root "sessions#home"
+
   get "/signup" => "users#new"
   post "/signup" => "users#create"
+
+  get '/login' => "sessions#new"
+  post '/login' => "sessions#create"
+
   resources :users
   resources :elves
   resources :letters
