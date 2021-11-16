03b016f05d9b35a70abaebb4e27bfa076486ac9e
“Daysha <+>
2021-11-15 20:17:25 -0800

    refactored error message for login route

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   get '/login' => "sessions#new"
   post '/login' => "sessions#create"
 
+  delete "/logout" => "sessions#logout"
+
   resources :users
   resources :elves
   resources :letters
