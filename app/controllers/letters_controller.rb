cfbd92b194d348476cca30cf961abedf5934b77a
“Daysha <+>
2021-11-22 09:56:26 -0800

    added new letter route and view

--- app/controllers/letters_controller.rb
+++ app/controllers/letters_controller.rb
@@ -4,4 +4,36 @@ class LettersController < ApplicationController
         @letter = Letter.all
     end
 
+    def new
+        @letter = Letter.new
+    end
+
+    def create
+        @letter = current_user.letters.new(letter_params)
+        if @letter.save
+            redirect_to letters_path, notice: "Letter has been added to your collection."
+        else
+            render :new
+        end
+    end
+
+    def edit
+
+    end
+
+    def update
+
+    end
+
+
+    def destroy
+
+    end
+
+    private
+
+    def letter_params
+
+    end
+
 end
