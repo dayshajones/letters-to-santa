70f5c5f71f3ab988ad888be81e3d5675704010f4
“Daysha <+>
2021-12-07 11:08:31 -0800

    refactoring user show page

--- app/helpers/letters_helper.rb
+++ app/helpers/letters_helper.rb
@@ -1,5 +1,13 @@
 module LettersHelper
 
+    def nested_form_conditional(f)
+        if @elf
+            f.hidden_field :elf_id, value: @elf.id
+            content_tag(:h4, "Write a letter to Santa with help from #{@elf.name}!!")
+        else
+            render partial: "nested_elf", locals: {f: f, e: @elf}
+        end
+    end
 
 end
 
